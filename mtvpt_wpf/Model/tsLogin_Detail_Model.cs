0e1009d03ea0ec9cc23ddde7fbb8778a8ef1c1aa
DESKTOP-LEH8K56\tan <tanwuttisakdanon@gmail.com>
2018-08-12 21:53:11 +0700

    Update All

---
+++ mtvpt_wpf/Model/tsLogin_Detail_Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtvpt_wpf.Model
+{
+    public class tsLogin_Detail_Model
+    {
+        public string login_id { get; set; }
+
+        public string login_username { get; set; }
+
+        public string login_password { get; set; }
+
+        public string login_date { get; set; }
+
+        public string login_time { get; set; }
+
+        public string login_ip { get; set; }
+
+        public string login_token { get; set; }
+    }
+}
