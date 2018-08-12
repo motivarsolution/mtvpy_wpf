4203a0f8d12cf5d9bdbc24ebe67adea15d5b4d9f
DESKTOP-LEH8K56\tan <tanwuttisakdanon@gmail.com>
2018-08-12 15:02:06 +0700

    Update Database and Model

---
+++ mtvpt_wpf/Model/zAccount_Detail_Model.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtvpt_wpf.Model
+{
+    class zAccount_Detail_Model
+    {
+        public string account_id { get; set; }
+
+        public string account_username { get; set; }
+
+        public string account_password { get; set; }
+
+        public string account_created_date { get; set; }
+
+        public string account_created_by { get; set; }
+
+        public string account_firstname { get; set; }
+
+        public string account_lastname { get; set; }
+
+        public string account_lastlogin_time { get; set; }
+
+        public string account_lastlogin_date { get; set; }
+
+        public string account_language_default { get; set; }
+
+        public string account_role_id { get; set; }
+
+        public string account_expire_date { get; set; }
+
+        public string account_status { get; set; }
+    }
+}
---
+++ mtvpt_wpf/Model/zSystem_Detail_Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtvpt_wpf.Model
+{
+    public class zSystem_Detail_Model
+    {
+        public string SYSTEM_ID { get; set; }
+
+        public string SYSTEM_NAME { get; set; }
+
+        public string SYSTEM_FULLNAME { get; set; }
+
+        public string SYSTEM_VERSION { get; set; }
+
+        public string SYSTEM_VERSION_NAME { get; set; }
+
+        public string SYSTEM_TIMEZONE { get; set; }
+
+        public string SYSTEM_LAST_SYNC_TIME { get; set; }
+
+        public string SYSTEM_LAST_SYNC_DATE { get; set; }
+    }
+}
